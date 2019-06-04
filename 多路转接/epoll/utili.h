4ef791cc47f0a6b45845a4a0a7ff9452d7e102b4
Timecur <45092718+Timecur@users.noreply.github.com>
2019-06-04 22:24:24 +0800

    Create utili.h

---
+++ 多路转接/epoll/utili.h
@@ -0,0 +1,18 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<assert.h>
+#include<sys/select.h>
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT  8787
+#define LISTEN_QUEUE 5
+#define SIZE 10
+#define BUFFER_SIZE 256#include<poll.h>
+#define OPEN_MAX 1000
+#include<sys/epoll.h>
+#define FDSIZE      1000
+#define EPOLLEVENTS 100 
