a11fbda72e5e6f1a8eb7853bbe836646029513ec
Timecur <45092718+Timecur@users.noreply.github.com>
2019-07-01 21:59:36 +0800

    Create testJson.cc

---
+++ Item/test/testJson.cc
@@ -0,0 +1,24 @@
+// 序列化测试
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <json/json.h>
+int main()
+{
+  Json::Value root;
+  Json::StreamWriterBuilder wb;
+  std::ostringstream os;
+  
+  root["Name"] = "zhangsan";
+  root["Age"] = 26;
+  root["Lang"] = "c++";
+  
+  std::unique_ptr<Json::StreamWriter> jsonWriter(wb.newStreamWriter());
+  jsonWriter->write(root, &os);
+  std::string s = os.str();
+  
+  std::cout << s << std::endl;
+  
+  return 0;
+}
