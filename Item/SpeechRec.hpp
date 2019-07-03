2294dc009f4173b0d53970fe3fc2cdfcc0f178a0
Timecur <45092718+Timecur@users.noreply.github.com>
2019-07-03 23:16:07 +0800

    Update SpeechRec.hpp

--- Item/SpeechRec.hpp
+++ Item/SpeechRec.hpp
@@ -25,5 +25,34 @@ class InterRobot{
 };
 //SpeechRec 完成核心逻辑
 class SpeechRec{
-};
+private:
+SpeechRec sr;
+InterRobot robot;
+std::unordered_map<std::string, std::string> command_set;
+public:
+//加载命令执行配置文件
+Jarvis()
+{
+char buffer[256];
+std::ifstream in(CMD_ETC);
+if(!in.is_open()){
+std::cerr << "open file error" <<std::endl;
+exit(1);
+}
+std::string sep = ":";
+while(in.getline(buffer, sizeof(buffer))){
+std::string str = buffer;
+std::size_t pos = str.find(sep);
+if(std::string::npos == pos){
+std::cerr << "Load Etc Error" << std::endl;
+exit(2);
+}
+std::string k = str.substr(0, pos);
+std::string v = str.substr(pos+sep.size());
+k+="。";
+command_set.insert(std::make_pair(k, v));
+}
+std::cout << "Load command etc ... done" << std::endl;
+in.close();
+;
 #endif
